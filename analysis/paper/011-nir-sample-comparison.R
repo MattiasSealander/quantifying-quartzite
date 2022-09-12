f44325eeac618d38c19b3e5f729f623cc87d786e
Mattias Sjölander <mattias.sjolander@umu.se>
2022-09-12 15:20:37 +0200

    Updated script, added raman spectra to figure as well as changed one sample for more appropriate example

--- analysis/paper/011-nir-sample-comparison.R
+++ analysis/paper/011-nir-sample-comparison.R
@@ -1,4 +1,7 @@
 # Loading package
+library(png)
+library(grid)
+library(jpeg)
 suppressPackageStartupMessages(library(cowplot))
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(flextable))
@@ -12,6 +15,10 @@ metadata.csv <-
 #Import nir data, set empty fields to NA
 nir.csv <-
   read.csv2("./analysis/data/raw_data/NIR/asd_raw_data_20220407.csv", sep = ";", dec = ".", header = TRUE, check.names = FALSE, na = c("","NA","NULL",NULL))
+
+#Import raman data, set empty fields to NA
+raman.csv <-
+  read.csv2("./analysis/data/raw_data/RAMAN/raman_samples_data_non_treated_20220407.csv", sep = ";", dec = ",", header = TRUE, check.names = FALSE, na = c("","NA","NULL",NULL))
 
 #Import xrf data, set empty fields to NA
 xrf.csv <-
@@ -29,6 +36,10 @@ nir.merged <-
 xrf.merged <- 
   as.data.frame(merge(metadata.csv, xrf.csv, by='sample_id'))
 
+#aggregate observations by group(sample) and calculate average of wavelength measurements
+raman.averaged <- 
+  aggregate(raman.csv[, 5:469], list(sample_id = raman.csv$sample_id), mean)
+
 #Filter nir data to focus on points and preforms made from quartz/quartzite material 
 Points.nir <-
   nir.merged %>%
@@ -43,11 +54,85 @@ Points.nir <-
                 type == "Point" | type == "Point fragment" | type == "Preform", 
                 material == "Brecciated quartz" | material == "Quartz" | material == "Quartzite") 
 
+#select sample_id and data columns and transpose in prep for baseline de-trend
+raman.transposed <- 
+  raman.averaged %>% 
+  dplyr::select(sample_id, `92.88`:`2503.59`)
+
+raman.transposed <- 
+  t(raman.transposed)
+
+#Write csv with transposed raman data for baseline de-trending in ChemoSpec package
+#ChemoSpec requires data to be read from file
+write.table(raman.transposed, file = "./analysis/data/derived_data/raman_transposed.csv", col.names = FALSE, sep = ";", dec = ".")
+
+#prepare vector with sample_id for reading raman data into spectra object
+sample_id <- 
+  raman.averaged$sample_id
+
+#read transposed raman data into spectra object
+raman <- suppressWarnings(matrix2SpectraObject(
+  gr.crit = sample_id,
+  gr.cols = c("auto"),
+  freq.unit ="Wavelength cm-1",
+  int.unit ="Intensity",
+  descrip ="Bifacial points measurements",
+  in.file = "./analysis/data/derived_data/raman_transposed.csv",
+  sep = ";",
+  dec = ".",
+  chk = TRUE,
+  out.file = "./analysis/data/derived_data/raman_spec_object"))
+
+#call on relevant baseline method (modified polynomial fitting) from baseline package and 
+#return corrected spectra to spectra object
+raman.baseline <- 
+  baselineSpectra(raman,
+                  int = FALSE,
+                  method = "modpolyfit",
+                  retC = TRUE)
+
+#prevent baselineSpectra from plotting a figure which will show up in the knitted document
+dev.off()
+
+#spectra are stored in a "Spectra" object, not a data frame
+#need to extract samples and frequencies from that object 
+raman.tmp <- 
+  #bind data from spectra object
+  as.data.frame(cbind(raman.baseline$freq, t(raman.baseline$data))) %>% 
+  #transpose data so that frequencies are column headers and observations rows
+  tibble::rownames_to_column() %>% 
+  pivot_longer(-rowname) %>% 
+  pivot_wider(names_from=rowname, values_from=value) %>%
+  #remove unnecessary column with previous column headers
+  dplyr::select(-name) %>% 
+  #Set frequencies as headers, as they are still first row
+  purrr::set_names(as.character(slice(., 1))) %>%
+  slice(-1) %>% 
+  #add sample id from spectra object
+  add_column(sample_id = sub("^X*", "", raman.baseline$names))
+
+#merge raman de-trended data with metadata
+raman.baseline.merged <- 
+  as.data.frame(merge(metadata.csv, raman.tmp, by='sample_id'))
+
+Points.raman <-
+  raman.baseline.merged %>%
+  filter(site_id == "Vilhelmina 1069" | site_id == "Vilhelmina 109" | site_id == "Vilhelmina 112" | site_id == "Vilhelmina 1124" | site_id == "Vilhelmina 1127" | site_id == "Vilhelmina 114" |
+           site_id == "Vilhelmina 115" | site_id == "Vilhelmina 117" | site_id == "Vilhelmina 118" | site_id == "Vilhelmina 1254" | site_id == "Vilhelmina 216" | site_id == "Vilhelmina 235" |
+           site_id == "Vilhelmina 240" | site_id == "Vilhelmina 245" | site_id == "Vilhelmina 252" | site_id == "Vilhelmina 263" | site_id == "Vilhelmina 335" | site_id == "Vilhelmina 356" |
+           site_id == "Vilhelmina 399" | site_id == "Vilhelmina 411" | site_id == "Vilhelmina 419" | site_id == "Vilhelmina 439" | site_id == "Vilhelmina 444" | site_id == "Vilhelmina 450" |
+           site_id == "Vilhelmina 458" | site_id == "Vilhelmina 539" | site_id == "Vilhelmina 542" | site_id == "Vilhelmina 611" | site_id == "Vilhelmina 619" | site_id == "Vilhelmina 636" | 
+           site_id == "Vilhelmina 637" | site_id == "Vilhelmina 643" | site_id == "Vilhelmina 769" | site_id == "Vilhelmina 949" | site_id == "Vilhelmina 95" | site_id == "Åsele 101" | 
+           site_id == "Åsele 107" | site_id == "Åsele 115" | site_id == "Åsele 117" | site_id == "Åsele 119" | site_id == "Åsele 129" | site_id == "Åsele 182" | site_id == "Åsele 188" |
+           site_id == "Åsele 393" | site_id == "Åsele 56" | site_id == "Åsele 91" | site_id == "Åsele 92" | site_id == "Åsele 99", 
+         type == "Point" | type == "Point fragment" | type == "Preform", 
+         material == "Brecciated quartz" | material == "Quartz" | material == "Quartzite")
+
 #Filter xrf data to focus on points and preforms made from quartz/quartzite material 
 Points.xrf <-
   xrf.merged %>%
   dplyr::select(sample_id, `Al`, `Si`, `K`, `Ca`, `Fe`, `Zr`, `Ti`) %>% 
-  dplyr::filter(sample_id == 231 | sample_id == 249| sample_id == 391)
+  dplyr::filter(sample_id == 231 | sample_id == 404| sample_id == 391)
 
 ft <- 
   flextable(Points.xrf) %>% 
@@ -73,13 +158,13 @@ n.231.long <-
 
 #Filter NIR data to focus on colourless 
 #and select the NIR range 1 000 - 2 500 nm
-n.249 <- Points.nir %>%
+n.404 <- Points.nir %>%
   dplyr::filter(sample_id == 249) %>% 
   dplyr::select(1, c(681:2180))
 
 #Melt into long format
-n.249.long <- 
-  suppressWarnings(melt(setDT(n.249), variable.name = "Wavelength", variable.factor = FALSE, value.name = "Absorbance"))
+n.404.long <- 
+  suppressWarnings(melt(setDT(n.404), variable.name = "Wavelength", variable.factor = FALSE, value.name = "Absorbance"))
 
 #Filter NIR data to focus on colourless 
 #and select the NIR range 1 000 - 2 500 nm
@@ -92,7 +177,7 @@ n.391.long <-
   suppressWarnings(melt(setDT(n.391), variable.name = "Wavelength", variable.factor = FALSE, value.name = "Absorbance"))
 
 #plot the colourless spectra
-p.231 <-
+pn.231 <-
   ggplot(n.231.long, aes(x = as.numeric(Wavelength))) + 
   geom_line(aes(y = Absorbance, colour = ""), size = 1, stat = "identity") +
   xlab("Wavelength (nm)") +
@@ -104,36 +189,36 @@ p.231 <-
   theme_classic() +
   theme(legend.position = "none",
         plot.title = element_text(size=16, 
-                                  hjust = 0.02),        
+                                  hjust = .9),        
         axis.title.x = element_blank(),
         axis.title.y = element_text(size = 12, face = "bold", colour = "black"),
         legend.title = element_text(size = 12, face = "bold", colour = "black"))
 
 #plot the colourless spectra
-p.249 <-
-  ggplot(n.249.long, aes(x = as.numeric(Wavelength))) + 
+pn.404 <-
+  ggplot(n.404.long, aes(x = as.numeric(Wavelength))) + 
   geom_line(aes(y = Absorbance, colour = ""), size = 1, stat = "identity") +
   xlab("Wavelength (nm)") +
-  ylab("Absorbance") +
-  labs(title="249") +
-  scale_color_manual(name = "249",
+  #ylab("Absorbance") +
+  labs(title="404") +
+  scale_color_manual(name = "404",
                      values = "black") + 
   scale_x_continuous(limits = c(1000, 2500), breaks = scales::pretty_breaks(n = 5)) +
   theme_classic() +
   theme(legend.position = "none",
         plot.title = element_text(size=16, 
-                                  hjust = 0.02),
+                                  hjust = .9),
         axis.title.x = element_blank(),
-        axis.title.y = element_text(size = 12, face = "bold", colour = "black"),
+        axis.title.y = element_blank(),
         legend.title = element_text(size = 12, face = "bold", colour = "black"))
 
 
 #plot the colourless spectra
-p.391 <-
+pn.391 <-
   ggplot(n.391.long, aes(x = as.numeric(Wavelength))) + 
   geom_line(aes(y = Absorbance, colour = ""), size = 1, stat = "identity") +
   xlab("Wavelength (nm)") +
-  ylab("Absorbance") +
+  #ylab("Absorbance") +
   labs(title="391") +
   scale_color_manual(name = "391",
                      values = "black") + 
@@ -141,20 +226,110 @@ p.391 <-
   theme_classic() +
   theme(legend.position = "none",
         plot.title = element_text(size=16, 
-                                  hjust = 0.02),
+                                  hjust = .9),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_text(size = 12, face = "bold", colour = "black"))
+
+#Filter raman data for sample 231 
+r.231 <- Points.raman %>%
+  filter(sample_id == 231) %>% 
+  dplyr::select(1, c(30:494))
+
+#Melt into long format
+r.231.long <- 
+  suppressWarnings(reshape2::melt(data.table::setDT(r.231), variable.name = "Wavenumber", variable.factor = FALSE, value.name = "Intensity"))
+
+#Filter raman data for sample 249 
+r.404 <- Points.raman %>%
+  filter(sample_id == 404) %>% 
+  dplyr::select(1, c(30:494))
+
+#Melt into long format
+r.404.long <- 
+  suppressWarnings(reshape2::melt(data.table::setDT(r.404), variable.name = "Wavenumber", variable.factor = FALSE, value.name = "Intensity"))
+
+#Filter raman data for sample 391
+r.391 <- Points.raman %>%
+  filter(sample_id == 391) %>% 
+  dplyr::select(1, c(30:494))
+
+#Melt into long format
+r.391.long <- 
+  suppressWarnings(reshape2::melt(data.table::setDT(r.391), variable.name = "Wavenumber", variable.factor = FALSE, value.name = "Intensity"))
+
+#plot the 231 spectra
+pr.231 <-
+  ggplot(r.231.long, aes(x = as.numeric(as.character(Wavenumber)))) + 
+  geom_line(aes(y = Intensity, colour = "", group = sample_id), size = 1, stat = "identity") +
+  xlab("Wavenumber (cm-1)") +
+  ylab("Intensity") +
+  labs(title="231") +
+  scale_color_manual(name = "Dark",
+                     values = "black") + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(size=16, 
+                                  hjust = .9),
         axis.title.x = element_blank(),
         axis.title.y = element_text(size = 12, face = "bold", colour = "black"),
         legend.title = element_text(size = 12, face = "bold", colour = "black"))
 
+#plot the 249 spectra
+pr.404 <-
+  ggplot(r.404.long, aes(x = as.numeric(as.character(Wavenumber)))) + 
+  geom_line(aes(y = Intensity, colour = "", group = sample_id), size = 1, stat = "identity") +
+  xlab("Wavenumber (cm-1)") +
+  ylab("Intensity") +
+  labs(title="404") +
+  scale_color_manual(name = "Dark",
+                     values = "black") + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(size=16, 
+                                  hjust = .9),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_text(size = 12, face = "bold", colour = "black"))
 
+#plot the 391 spectra
+pr.391 <-
+  ggplot(r.391.long, aes(x = as.numeric(as.character(Wavenumber)))) + 
+  geom_line(aes(y = Intensity, colour = "", group = sample_id), size = 1, stat = "identity") +
+  xlab("Wavenumber (cm-1)") +
+  ylab("Intensity") +
+  labs(title="391") +
+  scale_color_manual(name = "Dark",
+                     values = "black") + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.title = element_text(size=16, 
+                                  hjust = .9),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_text(size = 12, face = "bold", colour = "black"))
+
+img.231 <- readJPEG("./analysis/figures/231.JPG",  native=TRUE)
+g.231 <- rasterGrob(img.231, interpolate=TRUE)
+
+img.404 <- readJPEG("./analysis/figures/404.JPG",  native=TRUE)
+g.404 <- rasterGrob(img.404, interpolate=TRUE)
+
+img.391 <- readJPEG("./analysis/figures/391.JPG",  native=TRUE)
+g.391 <- rasterGrob(img.391, interpolate=TRUE)
 
 top <- 
   plot_grid(x.ft)
-bottom <- 
-  plot_grid(p.231, p.249, p.391,nrow=1)
+p.nir <- 
+  plot_grid(pn.231, pn.391, pn.404, labels = "NIRS", nrow=1)
+p.raman <- 
+  plot_grid(pr.231, pr.391, pr.404, labels = "Raman", nrow=1)
+pics <- 
+  plot_grid(g.231, g.391, g.404, nrow=1)
+
 
 fig <- 
-  cowplot::plot_grid(top,bottom, nrow = 2)
+  cowplot::plot_grid(top,p.nir,p.raman,pics, nrow = 4, align="hv")
 
 #Save figure
 ggsave("011-nir-sample-comparison.png",
@@ -162,6 +337,6 @@ ggsave("011-nir-sample-comparison.png",
        device = "png",
        here::here("analysis/figures/"),
        width=25, 
-       height=10,
+       height=25,
        units = "cm",
        dpi = 300)
